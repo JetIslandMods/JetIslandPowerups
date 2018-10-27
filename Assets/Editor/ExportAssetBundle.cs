a8775d3a7ca1ce5e8f6f17a25048815fbc6ecd36
JetIslandMods <43924053+JetIslandMods@users.noreply.github.com>
2018-10-27 15:42:52 -0400

    Initial commit

---
+++ Assets/Editor/ExportAssetBundle.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ExportAssetBundle
+{
+    [MenuItem("Assets/Build AssetBundle")]
+    static void ExportResource()
+    {
+        string folderName = "AssetBundles";
+        string filePath = Path.Combine(Application.streamingAssetsPath, folderName);
+
+        //Build for Windows platform
+        BuildPipeline.BuildAssetBundles(filePath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+
+    private float _yOffset = 0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        _yOffset = Mathf.Sin(Time.time * 2f) * 0.5f * Time.deltaTime;
+        gameObject.transform.Rotate(new Vector3(0f, 90f * Time.deltaTime, 0f));
+        gameObject.transform.position += new Vector3(0f, _yOffset, 0f);
+    }
+}
